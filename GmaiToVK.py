53bf47f5b4a55977a512a326d2f8eb63af13c09c
Medvedev Denis <37630193+MedvedevDenis@users.noreply.github.com>
2019-01-01 01:15:10 +0400

    1

--- GmaiToVK.py
+++ GmaiToVK.py
@@ -1,56 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 00:04:29 2019
 
-# A very simple Flask Hello World app for you to get started with...
+@author: Medvedev Denis
+"""
 
-from flask import Flask,request,json
+
 import vk
+from configuration import *
+from requests import *
+import os
 import time
-import random
-import threading
-app = Flask(__name__)
 
-def get_message():
-    if random.randint(0,10)%2==0:
-        return True
-    return False
 
-@app.route('/')
-def hello_world():
-
-    return 'r'
-
-@app.route('/', methods=['POST'])
-def processing():
-    data = json.loads(request.data)
-    if 'type' not in data.keys():
-        return 'not vk'
-    if data['type'] == 'confirmation':
-        return '**'
-    
-    '''
-    Обработка входящих
-    '''
-    if data['type']== 'message_new':
-        session = vk.Session()
-        api = vk.API(session, v=5.84)
-        peer_id = data['object']['peer_id']
-        api.messages.send(access_token='**', peer_id=(peer_id), message='Вам письмо')
-        return 'ok'
-
-def Listener():
-    session = vk.Session()
-    api = vk.API(session, v=5.84)
-
-    '''
-    Тут должна быть авторизация GMAIL
-    '''
-    peer_id = '**'
-    while True:
-        if get_message()==True:
-            api.messages.send(access_token='**', peer_id=(peer_id), message='Вам письмо')
-        time.sleep(1)
-
-t = threading.Thread(target=Listener,
-                         name="Listener",
-                         args=(),
-                         daemon=True)
-t.start()
+def main():
+        session = vk.Session(access_token = VK_API_ACCESS_TOKEN)
+        api = vk.API(session, v = VK_API_VERSION)
+        
+        
+        '''
+            Перед подключением к Long Poll серверу необходимо получить данные сессии (server, key, ts) методом groups.getLongPollServer. 
+            getLongPollServer получает адрес для подключения к Long Poll серверу ВКонтакте;
+        ''' 
+        
+        longPoll = api.groups.getLongPollServer(group_id = GROUP_ID) #
+        server, key, ts = longPoll['server'], longPoll['key'], longPoll['ts']
+        peer_id=None
+        print('START')
+        STOP=True
+        while True:
+            
+            
+            '''
+            POST-запрос вида {$server}?act=a_check&key={$key}&ts={$ts}&wait=25 
+            
+            -key — секретный ключ сессии;
+            -server — адрес сервера;
+            -ts — номер последнего события, начиная с которого нужно получать данные;
+            -wait — время ожидания (так как некоторые прокси-серверы обрывают соединение после 30 секунд, мы рекомендуем указывать wait=25). Максимальное значение — 90. 
+            '''
+            longPoll = post('%s'%server, data = {'act': 'a_check',
+                                                 'key': key,
+                                                 'ts': ts,
+                                                 'wait': 25}).json()
+        
+            '''
+            JSON-объект в ответе содержит два поля:
+        
+            ts (integer) — номер последнего события. Используйте его в следующем запросе.
+            updates (array) — массив, элементы которого содержат представление новых событий.
+            '''
+            if longPoll['updates'] and len(longPoll['updates']) != 0:
+                for update in longPoll['updates']:
+                    
+                    '''
+                    Событие message_new- входящее сообщение
+                    '''
+                    if update['type'] == 'message_new':
+                        
+                        peer_id=update['object']['peer_id']
+                        
+                        '''
+                        Помечаем сообщение как прочитанное
+                        '''
+                        api.messages.markAsRead(peer_id = update['object']['peer_id'])
+                        
+                        if update['object']['text']=='СТАРТ':
+                            STOP=False
+                        if update['object']['text']=='СТОП':
+                            STOP=True
+                        '''
+                        Отправляем сообщение
+                        '''
+                        api.messages.send(peer_id = update['object']['peer_id'], message = 'С новым годом')
+                        
+            
+            if peer_id is not None and not STOP :
+                    time.sleep(1)
+                    api.messages.send(peer_id =peer_id, message = 'С новым годом')
+                
+            ts = longPoll['ts']
+main()
