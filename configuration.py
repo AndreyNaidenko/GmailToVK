fea69aa69bcc9b453aa164e524356e118d1770b1
Medvedev Denis <37630193+MedvedevDenis@users.noreply.github.com>
2019-01-01 15:32:40 +0400

    Переписал в ООП

---
+++ configuration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 00:04:29 2019
+
+@author: Medvedev Denis
+"""
+
+VK_API_ACCESS_TOKEN = '***'  
+GROUP_ID = '***'        
+VK_API_VERSION = '5.84'
+GMAIL_CLIENT_ID='client_id.json'
+GMAIL_SERVICE_ACCOUNT_KEY='**.json'
---
+++ scopes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 14:49:16 2019
+
+@author: Medvedev Denis
+"""
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+]
