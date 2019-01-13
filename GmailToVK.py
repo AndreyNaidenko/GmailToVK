342038d9af5a89a6eb02f7cf0c67c985250c618f
Medvedev Denis <37630193+MedvedevDenis@users.noreply.github.com>
2019-01-13 19:57:18 +0400

    Поправлены комментарии. Добавлены docstring

--- GmailToVK.py
+++ GmailToVK.py
@@ -17,6 +17,17 @@ class BotGmailToVk():
                  vk_api_version,
                  gmail_client_secret,
                  gmail_service_account_key=None):
+        """Функция инициализации.
+
+        Arguments:
+            vk_access_token {str} -- Токен сообщества vk.com
+            vk_api_version {str} -- Используемая версия api vk.com
+            gmail_client_secret {str} -- Путь к токену приложения gmail
+
+        Keyword Arguments:
+            gmail_service_account_key {[type]} -- [description] (default: {None})
+        """
+
         self.vk_api_version = vk_api_version
         self.gmail_client_secret = gmail_client_secret
         self.gmail_service_account_key = gmail_service_account_key
@@ -25,8 +36,15 @@ class BotGmailToVk():
         self.vk_api = vk.API(self.vk_session, v=vk_api_version)
 
     def connect_to_vk_long_poll(self, group_id):
+        """Подключение к long poll серверу vk.
+
+        Arguments:     group_id {str} -- Id группы используемой для
+        рассылки
+        """
+
         try:
-            self.longPoll = self.vk_api.groups.getLongPollServer(group_id=group_id)
+            self.longPoll = self.vk_api.groups.getLongPollServer(
+                group_id=group_id)
             self.server = self.longPoll['server']
             self.key = self.longPoll['key']
             self.ts = self.longPoll['ts']
@@ -34,33 +52,55 @@ class BotGmailToVk():
             print("Failed :" + str(e))
 
     def create_vk_id_base(self):
+        """Создание базы sqlite, хранящей id пользователей vk.com."""
+
         e = create_engine("sqlite:///vk_id.db")
         e.execute("create table vk_id (peer_id integer primary key)")
 
     def add_to_vk_private_messages(self, peer_id):
+        """Добавление id пользователя в базу.
+
+        Arguments:     peer_id {str} -- Id пользователя
+        """
+
         if not os.path.exists("vk_id.db"):
             self.create_vk_id_base()
         e = create_engine("sqlite:///vk_id.db")
         try:
-            e.execute("INSERT INTO `vk_id`(`peer_id`) VALUES (" + str(peer_id) +  ");")
+            e.execute("INSERT INTO `vk_id`(`peer_id`) VALUES (" +
+                      str(peer_id) + ");")
         except Exception as e:
             print("\tFailed : +" + str(e) + "\r\n")
 
     def send_vk_private_messages(self, message):
+        """Рассылка сообщений пользователям из базы.
+
+        Arguments:     message {str} -- Сообщение которое будет
+        отправлено пользователям
+        """
+
         if os.path.exists("vk_id.db"):
             e = create_engine("sqlite:///vk_id.db")
             peer_id = e.execute("SELECT peer_id FROM vk_id;").fetchall()
             for p_id in peer_id:
-                self.vk_api.messages.send(peer_id=p_id[0], message=message)
+                self.vk_api.messages.send(
+                    peer_id=p_id[0], random_id='0', message=message)
 
     def connect_to_gmail(self, scopes):
+        """Подключение к gmail.
+
+        Arguments:     scopes {list} -- Области доступа
+        """
+
         try:
             store = file.Storage('token.json')
             creds = store.get()
             if not creds or creds.invalid:
-                flow = client.flow_from_clientsecrets('client_secret.json', scopes)
+                flow = client.flow_from_clientsecrets('client_secret.json',
+                                                      scopes)
                 creds = tools.run_flow(flow, store)
-            self.gmail_service = build('gmail', 'v1', http=creds.authorize(Http()))
+            self.gmail_service = build(
+                'gmail', 'v1', http=creds.authorize(Http()))
 
             # получаем email-адрес авторизованного пользователя
             self.gmail_user = self.gmail_service.users().getProfile(
@@ -73,6 +113,12 @@ class BotGmailToVk():
             print("User:", self.gmail_user['emailAddress'], '\r\n')
 
     def get_last_message(self, user_id='me'):
+        """Получение последнего сообщения в gmail.com.
+
+        Keyword Arguments:     user_id {str} -- Id в gmail (default:
+        {'me'})
+        """
+
         self.history = self.gmail_service.users().history().list(
             userId='me',
             historyTypes=['messageAdded'],
@@ -83,13 +129,20 @@ class BotGmailToVk():
                 messages = h['messages']
                 for message in messages:
                     self.last_message = self.gmail_service.users().messages(
-                        ).get(userId='me', id=message['id']).execute()
+                    ).get(
+                        userId='me', id=message['id']).execute()
         except Exception as e:
             print("Failed :" + str(e))
             self.last_message = None
         self.historyId = self.history['historyId']
 
     def gmail_log_out(self, filename):
+        """Выход из аккаунта gmail.com.
+
+        Arguments:     filename {str} -- Путь к файлу в котором хранится
+        токен
+        """
+
         try:
             os.remove(filename)
         except:
@@ -99,18 +152,20 @@ class BotGmailToVk():
             print('\tLogout completed')
 
     def run(self):
+        """Основная функция."""
+
         peer_id = None
         STOP = True
         SERVER = True
         while SERVER:
             try:
-                '''
-                POST-запрос вида {$server}?act=a_check&key={$key}&ts={$ts}&wait=25
-                -key — секретный ключ сессии;
-                -server — адрес сервера;
-                -ts — номер последнего события, начиная с которого нужно получать данные;
-                -wait — время ожидания (так как некоторые прокси-серверы обрывают соединение после 30 секунд, мы рекомендуем указывать wait=25). Максимальное значение — 90.
-                '''
+
+                #POST-запрос вида {$server}?act=a_check&key={$key}&ts={$ts}&wait=25
+                #-key — секретный ключ сессии;
+                #-server — адрес сервера;
+                #-ts — номер последнего события, начиная с которого нужно получать данные;
+                #-wait — время ожидания (так как некоторые прокси-серверы обрывают соединение после 30 секунд, мы рекомендуем указывать wait=25). Максимальное значение — 90.
+
                 self.longPoll = post(
                     '%s' % self.server,
                     data={
@@ -119,16 +174,18 @@ class BotGmailToVk():
                         'ts': self.ts,
                         'wait': 25
                     }).json()
-                '''
-                JSON-объект в ответе содержит два поля:
-                ts (integer) — номер последнего события. Используйте его в следующем запросе.
-                updates (array) — массив, элементы которого содержат представление новых событий.
-                '''
+
+                #JSON-объект в ответе содержит два поля:
+                #ts (integer) — номер последнего события. Используйте его в следующем запросе.
+                #updates (array) — массив, элементы которого содержат представление новых событий.
+
                 if self.longPoll['updates'] and len(
                         self.longPoll['updates']) != 0:
                     for update in self.longPoll['updates']:
-                        if update['type'] == 'message_new': # Событие message_new- входящее сообщение
-                            peer_id = update['object']['peer_id'] # Получаем id пользователя, отправившего сообщение боту
+                        if update[
+                                'type'] == 'message_new':  # Событие message_new- входящее сообщение
+                            peer_id = update['object'][
+                                'peer_id']  # Получаем id пользователя, отправившего сообщение боту
                             if 'старт' in update['object']['text'].lower():
                                 STOP = False
                                 self.vk_api.messages.send(
@@ -141,7 +198,8 @@ class BotGmailToVk():
                                     peer_id=peer_id,
                                     random_id='0',
                                     message='Бот остановлен')
-                            if 'остановить' in update['object']['text'].lower(): # Остановка бота, выход из while True:
+                            if 'остановить' in update['object']['text'].lower(
+                            ):  # Остановка бота, выход из while True:
                                 SERVER = False
                                 self.vk_api.messages.send(
                                     peer_id=peer_id,
@@ -156,7 +214,8 @@ class BotGmailToVk():
 
                             if 'отправляй мне в лс' in update['object'][
                                     'text'].lower():
-                                self.add_to_vk_private_messages(update['object']['from_id'])
+                                self.add_to_vk_private_messages(
+                                    update['object']['from_id'])
                                 self.vk_api.messages.send(
                                     peer_id=update['object']['from_id'],
                                     random_id='0',
@@ -165,21 +224,27 @@ class BotGmailToVk():
                 if peer_id is not None or not STOP:
                     self.get_last_message(user_id='me')
                     if self.last_message:
-                        subject = "" # если письмо будет без темы, то
-                                     # эта строка просто останется пустой
+                        subject = ""  # если письмо будет без темы, то
+                        # эта строка просто останется пустой
                         # постепенно углубляемся в словарь для получения данных
                         main_headers = self.last_message['payload']
                         headers = main_headers['headers']
 
-                        for item in headers: # цикл по словарям
-                            if item['name'] == 'From': # ищем From - уникальное name с данными автора
-                                author = "Автор: " + item['value'] + "\n" # строка с автором и почтой письма
-                            if item['name'] == 'Subject': # ищем Subject - уникальное name с темой
-                                subject = "Тема: " + item['value'] # формируем строку с темой письма
+                        for item in headers:  # цикл по словарям
+                            if item['name'] == 'From':  # ищем From - уникальное name с данными автора
+                                author = "Автор: " + item[
+                                    'value'] + "\n"  # строка с автором и почтой письма
+                            if item['name'] == 'Subject':  # ищем Subject - уникальное name с темой
+                                subject = "Тема: " + item[
+                                    'value']  # формируем строку с темой письма
 
-                        vk_message = "На почте новое письмо\n" +  author + subject # формируем строку для отправки в вк
-                        if "INBOX" in self.last_message['labelIds']: # если в ответе на запрос есть метка "INBOX"
-                            self.vk_api.messages.send(peer_id=VK_CHAT_ID,random_id=0, message=vk_message) # отправляем в вк
+                        vk_message = "На почте новое письмо\n" + author + subject  # формируем строку для отправки в вк
+                        if "INBOX" in self.last_message[
+                                'labelIds']:  # если в ответе на запрос есть метка "INBOX"
+                            self.vk_api.messages.send(
+                                peer_id=VK_CHAT_ID,
+                                random_id='0',
+                                message=vk_message)  # отправляем в вк
                             self.send_vk_private_messages(vk_message)
                 self.ts = self.longPoll['ts']
             except Exception as e:
@@ -187,7 +252,8 @@ class BotGmailToVk():
                 SERVER = False
 
 
-gmvkbot = BotGmailToVk(VK_API_ACCESS_TOKEN, VK_API_VERSION, GMAIL_CLIENT_SECRET)
+gmvkbot = BotGmailToVk(VK_API_ACCESS_TOKEN, VK_API_VERSION,
+                       GMAIL_CLIENT_SECRET)
 gmvkbot.connect_to_gmail(SCOPES)
 gmvkbot.connect_to_vk_long_poll(GROUP_ID)
 gmvkbot.run()
